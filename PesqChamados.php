69c7c49d6937c9a3f31bae7370ad5b0b523e46a6
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2023-11-18 19:34:52 -0300

    Update PesqChamados.php - v.Correta

--- PesqChamados.php
+++ PesqChamados.php
@@ -8,7 +8,15 @@
 </head>
 <body>
     <center>
-        <h1>Chamados registrados</h1>
+        <h1>Chamados abertos e fechados</h1>
+
+        <form method="post" action="PesqChamados.php">
+            <label for="ra">RA do aluno:</label>
+            <input type="text" name="ra" id="ra" required>
+            <input type="submit" value="Pesquisar">
+            <br><br>
+        </form>
+
         <table border="1">
             <tr>
                 <td>ID</td>
@@ -24,22 +32,27 @@
             <?php
                 require("conectar.php");
 
-                $dados_select = mysqli_query($conn, "SELECT id_chamado, hora_cadastro, ra, nome, sala, descricao, situacao, hora_resolvidos, problemas_chamados.tipo_problema FROM chamados_cadastrados INNER JOIN problemas_chamados ON problemas_chamados.id_problema = chamados_cadastrados.problema ORDER BY id_chamado DESC");
-                echo "<form>";
-                while($dado = mysqli_fetch_array($dados_select)) {
-                        echo '<tr>';
-                        echo '<td>'.$dado[0].'</td>';
-                        echo '<td>'.$dado[1].'</td>';
-                        echo '<td>'.$dado[2].'</td>';
-                        echo '<td>'.$dado[3].'</td>';
-                        echo '<td>'.$dado[4].'</td>';
-                        echo '<td>'.$dado[8].'</td>';
-                        echo '<td>'.$dado[5].'</td>';
-                        echo '<td>'.$dado[6].'</td>';
-                        echo '<td>'.$dado[7].'</td>';
-                        echo '</tr>';
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if (isset($conn)) {
+                        $raPesquisa = mysqli_real_escape_string($conn, $_POST['ra']);
+
+                        $dados_select = mysqli_query($conn, "SELECT id_chamado, hora_cadastro, ra, nome, sala, descricao, situacao, hora_resolvidos, problemas_chamados.tipo_problema FROM chamados_cadastrados INNER JOIN problemas_chamados ON problemas_chamados.id_problema = chamados_cadastrados.problema WHERE ra = '$raPesquisa' ORDER BY id_chamado DESC");
+
+                        while ($dado = mysqli_fetch_array($dados_select)) {
+                            echo '<tr>';
+                            echo '<td>'.$dado[0].'</td>';
+                            echo '<td>'.$dado[1].'</td>';
+                            echo '<td>'.$dado[2].'</td>';
+                            echo '<td>'.$dado[3].'</td>';
+                            echo '<td>'.$dado[4].'</td>';
+                            echo '<td>'.$dado[8].'</td>';
+                            echo '<td>'.$dado[5].'</td>';
+                            echo '<td>'.$dado[6].'</td>';
+                            echo '<td>'.$dado[7].'</td>';
+                            echo '</tr>';
+                        }
+                    }
                 }
-                echo "</form>";
             ?>
         </table>
         <br />
@@ -47,4 +60,3 @@
     </center>
 </body>
 </html>
-
